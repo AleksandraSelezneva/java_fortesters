3eadca7dc0e91657fdc9826224a8e316baaea8dc
Aleksandra Selezneva <Alexandra.Didenko@t-systems.com>
2021-02-05 23:33:47 +0300

    Добавлено несколько функций

--- sandbox/src/main/java/ru/stqa/fortesters/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/fortesters/sandbox/MyFirstProgram.java
@@ -3,6 +3,24 @@ package ru.stqa.fortesters.sandbox;
 public class MyFirstProgram {
 
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        hello("world");
+        hello("user");
+        hello("Alexei");
+        double len = 5;
+        System.out.println("Площадь квадрата со стороной " + len + "=" + area(len));
+        double c = 4;
+        double d = 6;
+        System.out.println("Площадь прямоугольника со сторонами " + c + " и " + d + " = " + area(c,d));
+    }
+
+    public static void hello(String somebody) {
+        System.out.println("Hello," + somebody + "!");
+    }
+
+    public static double area (double l) {
+    return l * l;
+}
+    public static double area (double a, double b) {
+        return a * b;
     }
 }
