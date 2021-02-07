1d800d8f21d66fcd4031c82b8c2c578d110180b7
Aleksandra Selezneva <Alexandra.Didenko@t-systems.com>
2021-02-07 14:44:33 +0300

    Задание 2. Вторая реализация (метод в классе Point)

--- sandbox/src/main/java/ru/stqa/fortesters/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/fortesters/sandbox/MyFirstProgram.java
@@ -13,14 +13,11 @@ public class MyFirstProgram {
         Rectangle r = new Rectangle(4,6);
         System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
 
-        Point p1 = new Point(1,2);
-        Point p2 = new Point(3,4);
-        System.out.println("Расстояние между точками = " + distance(p1,p2));
+        Point p = new Point(1,2,3,4);
+        System.out.println("Расстояние между точками = " + p.distance());
     }
     public static void hello(String somebody) {
         System.out.println("Hello," + somebody + "!");
     }
-    public static double distance (Point p1, Point p2){
-        return Math.sqrt((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y));
-    }
+
 }
