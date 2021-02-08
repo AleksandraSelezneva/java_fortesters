e03d1382c1679683e8626394e04fbc9841cfd9a2
Aleksandra Selezneva <Alexandra.Didenko@t-systems.com>
2021-02-08 15:11:25 +0300

    Задание 3. Первые тесты (тесты на расстояние отдельно)

--- sandbox/src/test/java/ru/stqa/fortesters/sandbox/SquareTests.java
+++ sandbox/src/test/java/ru/stqa/fortesters/sandbox/SquareTests.java
@@ -11,22 +11,4 @@ public class SquareTests {
         Square s = new Square(5);
         Assert.assertEquals(s.area(),25.0);
     }
-    @Test
-    public void testDistance1(){
-        Point p1 = new Point(1,2);
-        Point p2 = new Point(3,4);
-        Assert.assertEquals(Point.distance(p1,p2),2.8284271247461903);
-    }
-    @Test
-    public void testDistance2(){
-        Point p1 = new Point(1,1);
-        Point p2 = new Point(10,3);
-        Assert.assertEquals(Point.distance(p1,p2),9.219544457292887);
-    }
-    @Test
-    public void testDistance3(){
-        Point p1 = new Point(0,0);
-        Point p2 = new Point(9,9);
-        Assert.assertEquals(Point.distance(p1,p2),12.727922061357855);
-    }
 }
