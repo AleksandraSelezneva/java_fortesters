6c5331af75e03c6b4d811cb5b2bd1beb2cabd765
Aleksandra Selezneva <Alexandra.Didenko@t-systems.com>
2021-02-09 00:05:01 +0300

    Задание 3. Первые тесты. Исправление

--- sandbox/src/test/java/ru/stqa/fortesters/sandbox/DistanceTests.java
+++ sandbox/src/test/java/ru/stqa/fortesters/sandbox/DistanceTests.java
@@ -10,18 +10,18 @@ public class DistanceTests {
     public void testDistance1(){
         Point p1 = new Point(1,2);
         Point p2 = new Point(3,4);
-        Assert.assertEquals(Point.distance(p1,p2),2.8284271247461903);
+        Assert.assertEquals(p1.distance(p2),2.8284271247461903);
     }
     @Test
     public void testDistance2(){
         Point p1 = new Point(1,1);
         Point p2 = new Point(10,3);
-        Assert.assertEquals(Point.distance(p1,p2),9.219544457292887);
+        Assert.assertEquals(p1.distance(p2),9.219544457292887);
     }
     @Test
     public void testDistance3(){
         Point p1 = new Point(0,0);
         Point p2 = new Point(9,9);
-        Assert.assertEquals(Point.distance(p1,p2),12.727922061357855);
+        Assert.assertEquals(p1.distance(p2),12.727922061357855);
     }
 }
