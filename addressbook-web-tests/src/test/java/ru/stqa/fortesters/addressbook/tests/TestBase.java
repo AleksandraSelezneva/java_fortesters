c0ad0295d02718905ff163dfd6ce444d42f8cf9e
Aleksandra Selezneva <Alexandra.Didenko@t-systems.com>
2021-02-15 14:41:59 +0300

    Лекция 3.4

--- addressbook-web-tests/src/test/java/ru/stqa/fortesters/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/fortesters/addressbook/tests/TestBase.java
@@ -1,13 +1,14 @@
 package ru.stqa.fortesters.addressbook.tests;
 
 import org.openqa.selenium.*;
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import ru.stqa.fortesters.addressbook.appmanager.ApplicationManager;
 
 public class TestBase {
 
-    protected final ApplicationManager app = new ApplicationManager();
+    protected final ApplicationManager app = new ApplicationManager(BrowserType.IE);
     protected WebDriver wd;
 
     @BeforeMethod(alwaysRun = true)
@@ -19,5 +20,4 @@ public class TestBase {
     public void tearDown() throws Exception {
         app.stop();
     }
-
 }
