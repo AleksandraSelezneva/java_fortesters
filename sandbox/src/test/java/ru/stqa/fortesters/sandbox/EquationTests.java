49ec77fc40bc5dea7a33a52d74e2379d9adc5843
Aleksandra Selezneva <Alexandra.Didenko@t-systems.com>
2021-02-15 11:04:32 +0300

    Лекция 3.2

--- sandbox/src/test/java/ru/stqa/fortesters/sandbox/EquationTests.java
+++ sandbox/src/test/java/ru/stqa/fortesters/sandbox/EquationTests.java
@@ -9,20 +9,35 @@ public class EquationTests {
     public void tests0(){
         Equation e = new Equation(1,1,1);
         Assert.assertEquals(e.rootNumber(),0);
-
     }
 
     @Test
     public void tests1(){
         Equation e = new Equation(1,2,1);
         Assert.assertEquals(e.rootNumber(),1);
-
     }
 
     @Test
     public void tests2(){
         Equation e = new Equation(1,5,6);
         Assert.assertEquals(e.rootNumber(),2);
+    }
 
+    @Test
+    public void testsLinear(){
+        Equation e = new Equation(0,1,1);
+        Assert.assertEquals(e.rootNumber(),1);
+    }
+
+    @Test
+    public void testsConstant(){
+        Equation e = new Equation(0,0,1);
+        Assert.assertEquals(e.rootNumber(),0);
+    }
+
+    @Test
+    public void testsZero(){
+        Equation e = new Equation(0,0,0);
+        Assert.assertEquals(e.rootNumber(),-1);
     }
 }
