0f379ebdb1b5a2dd9de134b9828be5a9dc93471e
Aleksandra Selezneva <Alexandra.Didenko@t-systems.com>
2021-02-16 13:40:23 +0300

    Лекция 3.6

--- addressbook-web-tests/src/test/java/ru/stqa/fortesters/addressbook/tests/ContactCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/fortesters/addressbook/tests/ContactCreationTest.java
@@ -8,7 +8,7 @@ public class ContactCreationTest extends TestBase {
   @Test
   public void testContactCreation() throws Exception {
     app.getContactHelper().initContactCreation();
-    app.getContactHelper().fillContactForm(new ContactData("Aleksandra", "Selezneva", "89217775533", null));
+    app.getContactHelper().fillContactForm(new ContactData("Aleksandra", "Selezneva", "89217775533", "pochta"));
     app.getContactHelper().submitContactCreation();
     app.getNavigationHelper().gotoHomePage();
     app.getSessionHelper().logout();
