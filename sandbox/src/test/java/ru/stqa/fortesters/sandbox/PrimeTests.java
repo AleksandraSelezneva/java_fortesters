1f6b9508c3203a2cf720c2e5cc589caaad06dcb9
Aleksandra Selezneva <Alexandra.Didenko@t-systems.com>
2021-02-25 14:30:49 +0300

    Лекция 4.1.

---
+++ sandbox/src/test/java/ru/stqa/fortesters/sandbox/PrimeTests.java
@@ -0,0 +1,25 @@
+package ru.stqa.fortesters.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PrimeTests {
+
+    @Test
+    public void testPrimeFast() {
+        Assert.assertTrue(Primes.isPrime(Integer.MAX_VALUE));
+    }
+
+
+    @Test
+    public void testPrimeLong() {
+        long n = Integer.MAX_VALUE;
+        Assert.assertTrue(Primes.isPrime(n));
+    }
+
+
+    @Test (enabled = false)
+    public void testNonPrime() {
+        Assert.assertFalse(Primes.isPrime(Integer.MAX_VALUE-2));
+    }
+}
