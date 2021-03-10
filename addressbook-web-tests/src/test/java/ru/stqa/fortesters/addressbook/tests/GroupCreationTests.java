a1caa567605bbebb7f31ab90708c5092ea2fc2b2
Aleksandra Selezneva <Alexandra.Didenko@t-systems.com>
2021-03-10 12:57:17 +0300

    Лекция 6.5. группы

--- addressbook-web-tests/src/test/java/ru/stqa/fortesters/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/fortesters/addressbook/tests/GroupCreationTests.java
@@ -3,24 +3,27 @@ package ru.stqa.fortesters.addressbook.tests;
 import org.testng.annotations.*;
 import ru.stqa.fortesters.addressbook.model.GroupData;
 import ru.stqa.fortesters.addressbook.model.Groups;
-
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreationTests extends TestBase {
 
-@DataProvider
-public Iterator<Object[]> validGroups() {
-    List<Object[]> list = new ArrayList<Object[]>();
-    list.add(new Object[]{new GroupData().withName("test1").withHeader("header 1").withFooter("footer 1")});
-    list.add(new Object[]{new GroupData().withName("test2").withHeader("header 2").withFooter("footer 2")});
-    list.add(new Object[]{new GroupData().withName("test3").withHeader("header 3").withFooter("footer 3")});
-    return list.listIterator();
-}
+    @DataProvider
+    public Iterator<Object[]> validGroups() throws IOException {
+        List<Object[]> list = new ArrayList<Object[]>();
+        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.csv")));
+        String line = reader.readLine();
+        while (line != null){
+            String [] split = line.split(";");
+            list.add (new Object[] {new GroupData().withName(split[0]).withHeader(split[1]).withFooter(split[2])});
+            line = reader.readLine();
+        }
+        return list.listIterator();
+    }
 
     @Test(dataProvider = "validGroups")
     public void testsGroupCreation(GroupData group) throws Exception {
@@ -31,10 +34,10 @@ public Iterator<Object[]> validGroups() {
         Groups after = app.group().all();
         assertThat(after, equalTo(
                 before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
-       // app.getSessionHelper().logout();
+        // app.getSessionHelper().logout();
     }
 
-    @Test
+    @Test (enabled = false)
     public void testsBadGroupCreation() throws Exception {
         app.goTo().groupPage();
         Groups before = app.group().all();
