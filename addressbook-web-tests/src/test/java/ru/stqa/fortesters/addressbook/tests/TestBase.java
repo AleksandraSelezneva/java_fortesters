102049992b14863de0ff3531db23728e19c4a12a
Aleksandra Selezneva <Alexandra.Didenko@t-systems.com>
2021-03-14 15:36:06 +0300

    Лекция 6.10 + Задание 14

--- addressbook-web-tests/src/test/java/ru/stqa/fortesters/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/fortesters/addressbook/tests/TestBase.java
@@ -1,14 +1,18 @@
 package ru.stqa.fortesters.addressbook.tests;
 
 import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.fortesters.addressbook.appmanager.ApplicationManager;
 
+import static org.openqa.selenium.remote.BrowserType.CHROME;
+
 public class TestBase {
 
-    protected static final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
+    protected static final ApplicationManager app
+            = new ApplicationManager(System.getProperty("browser",BrowserType.CHROME));
     protected WebDriver wd;
 
     @BeforeSuite(alwaysRun = true)
