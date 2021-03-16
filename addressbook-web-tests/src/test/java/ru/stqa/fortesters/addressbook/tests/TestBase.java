73c126988165057606bf3fb2d584d6d1d7c648e1
Aleksandra Selezneva <Alexandra.Didenko@t-systems.com>
2021-03-16 17:40:00 +0300

    Лекция 7.1.

--- addressbook-web-tests/src/test/java/ru/stqa/fortesters/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/fortesters/addressbook/tests/TestBase.java
@@ -9,12 +9,8 @@ import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.fortesters.addressbook.appmanager.ApplicationManager;
-import java.lang.reflect.Member;
 import java.lang.reflect.Method;
 import java.util.Arrays;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class TestBase {
 
@@ -28,20 +24,18 @@ public class TestBase {
     public void setUp() throws Exception {
         app.init();
     }
-
     @AfterSuite(alwaysRun = true)
     public void tearDown() throws Exception {
         app.stop();
     }
+
     @BeforeMethod
     public void logTestStart(Method m, Object[] p) {
         logger.info("Start test " + m.getName() + " with parameters " + Arrays.asList(p));
     }
 
-
     @AfterMethod(alwaysRun = true)
     public void logTestStop(Method m) {
         logger.info("Stop test " + m.getName());
-
     }
 }
